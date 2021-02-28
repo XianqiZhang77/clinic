fea0542eae7af5848e0054f0d1132aaf73d61046
wujq95 <wujq1214@gmail.com>
2021-02-27 20:18:05 -0500

    manger approve and reject backend code

---
+++ src/main/java/com/soen6841/demo/controller/ManagerController.java
@@ -0,0 +1,120 @@
+package com.soen6841.demo.controller;
+
+import com.soen6841.demo.domain.Doctor;
+import com.soen6841.demo.domain.Nurse;
+import com.soen6841.demo.domain.Patient;
+import com.soen6841.demo.domain.Status;
+import com.soen6841.demo.service.ManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ManagerController {
+
+    @Autowired
+    private ManagerService managerService;
+
+    @GetMapping("/getAllDoctorUnderReview")
+    public String getAllDoctorUnderReview(Model model) {
+        Iterable<Doctor> doctors = managerService.getDoctorByRegisterStatus(Status.wating);
+        model.addAttribute("doctors",doctors);
+        return "manager_doctor";
+    }
+
+    @GetMapping("/getAllNurseUnderReview")
+    public String getAllNurseUnderReview(Model model) {
+        Iterable<Nurse> nurses =  managerService.getNurseByRegisterStatus(Status.wating);
+        model.addAttribute("nurses",nurses);
+        return "manager_nurse";
+    }
+
+    @GetMapping("/getAllPatientUnderReview")
+    public String getAllPatientUnderReview(Model model) {
+        Iterable<Patient> patients =   managerService.getPatientByRegisterStatus(Status.wating);
+        model.addAttribute("patients",patients);
+        return "manager_patient";
+    }
+
+
+    @PostMapping("/approveDoctor/{doctorId}")
+    public String approveDoctor(@PathVariable String doctorId, Model model){
+        Doctor doctor = managerService.findDoctorById(doctorId);
+        if(doctor==null){
+            model.addAttribute("msg","can not find doctor");
+        }else {
+            managerService.approveDoctor(doctor);
+        }
+        Iterable<Doctor> doctors = managerService.getDoctorByRegisterStatus(Status.wating);
+        model.addAttribute("doctors",doctors);
+        return "manager_doctor";
+    }
+
+    @PostMapping("/rejectDoctor/{doctorId}")
+    public String rejectDoctor(@PathVariable String doctorId, Model model){
+        Doctor doctor = managerService.findDoctorById(doctorId);
+        if(doctor==null){
+            model.addAttribute("msg","can not find doctor");
+        }else {
+            managerService.rejectDoctor(doctor);
+        }
+        Iterable<Doctor> doctors = managerService.getDoctorByRegisterStatus(Status.wating);
+        model.addAttribute("doctors",doctors);
+        return "manager_doctor";
+    }
+
+    @PostMapping("/approveNurse/{nurseId}")
+    public String approveNurse(@PathVariable String nurseId, Model model){
+        Nurse nurse = managerService.findNurseById(nurseId);
+        if(nurse==null){
+            model.addAttribute("msg","can not find doctor");
+        }else {
+            managerService.approveNurse(nurse);
+        }
+        Iterable<Nurse> nurses =  managerService.getNurseByRegisterStatus(Status.wating);
+        model.addAttribute("nurses",nurses);
+        return "manager_nurse";
+    }
+
+    @PostMapping("/rejectNurse/{nurseId}")
+    public String rejectNurse(@PathVariable String nurseId, Model model){
+        Nurse nurse = managerService.findNurseById(nurseId);
+        if(nurse==null){
+            model.addAttribute("msg","can not find doctor");
+        }else {
+            managerService.rejectNurse(nurse);
+        }
+        Iterable<Nurse> nurses =  managerService.getNurseByRegisterStatus(Status.wating);
+        model.addAttribute("nurses",nurses);
+        return "manager_nurse";
+    }
+
+    @PostMapping("/approvePatient/{patientId}")
+    public String approvePatient(@PathVariable String patientId, Model model){
+        Patient patient = managerService.findPatientById(patientId);
+        if(patient==null){
+            model.addAttribute("msg","can not find doctor");
+        }else {
+            managerService.approvePatient(patient);
+        }
+        Iterable<Patient> patients =   managerService.getPatientByRegisterStatus(Status.wating);
+        model.addAttribute("patients",patients);
+        return "manager_patient";
+    }
+
+    @PostMapping("/rejectPatient/{patientId}")
+    public String rejectPatient(@PathVariable String patientId, Model model){
+        Patient patient = managerService.findPatientById(patientId);
+        if(patient==null){
+            model.addAttribute("msg","can not find doctor");
+        }else {
+            managerService.rejectPatient(patient);
+        }
+        Iterable<Patient> patients =   managerService.getPatientByRegisterStatus(Status.wating);
+        model.addAttribute("patients",patients);
+        return "manager_patient";
+    }
+}
---
+++ src/main/java/com/soen6841/demo/util/SystemListener.java
@@ -0,0 +1,72 @@
+package com.soen6841.demo.util;
+
+import com.soen6841.demo.dao.DoctorRepository;
+import com.soen6841.demo.dao.NurseRepository;
+import com.soen6841.demo.dao.PatientRepository;
+import com.soen6841.demo.domain.Doctor;
+import com.soen6841.demo.domain.Nurse;
+import com.soen6841.demo.domain.Patient;
+import com.soen6841.demo.domain.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class SystemListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    private NurseRepository nurseRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        if(getDoctorList()!=null){
+            doctorRepository.saveAll(getDoctorList());
+        }
+        if(getNurseList()!=null){
+            nurseRepository.saveAll(getNurseList());
+        }
+
+        if(getPatientList()!=null){
+            patientRepository.saveAll(getPatientList());
+        }
+
+    }
+
+    private List<Doctor> getDoctorList(){
+        List<Doctor> doctorList = new ArrayList<>();
+        Doctor doctor1 = new Doctor("doctor1","Bob","a@gamil.com","A St,","111222333",new Date(19999999),"889", Status.wating);
+        doctorList.add(doctor1);
+        Doctor doctor2 = new Doctor("doctor2","Mike","b@gamil.com","B St,","888866666",new Date(19999999),"123", Status.wating);
+        doctorList.add(doctor2);
+        Doctor doctor3 = new Doctor("doctor3","Lucy","c@gamil.com","C St,","768676678678",new Date(19999999),"188", Status.wating);
+        doctorList.add(doctor3);
+        return doctorList;
+    }
+
+    private List<Nurse> getNurseList(){
+        List<Nurse> nurseList = new ArrayList<>();
+        Nurse nurse1 = new Nurse("nurse1","Linda","k@gamil.com","L St,","54768558",new Date(19999999),"1992", Status.wating);
+        nurseList.add(nurse1);
+        Nurse nurse2 = new Nurse("nurse2","Bill","bikk@gamil.com","Bi St,","8888698699",new Date(288888888),"1236", Status.wating);
+        nurseList.add(nurse2);
+        return nurseList;
+    }
+
+    private List<Patient> getPatientList(){
+        List<Patient> patientList = new ArrayList<>();
+        Patient patient1 = new Patient("patient1","Will","ww@gamil.com","WWD St,","19999",new Date(19999999), Status.wating);
+        patientList.add(patient1);
+        return patientList;
+    }
+}
